4f209c751536b36182cf54eefacf53c98b177738
emericriv <e19riviere@gmail.com>
2024-09-16 16:56:53 +0200

    All animations are working well index_html is a copy of index.html with elements divided which is better organised for the HTML, can be used in future. script.js is updated with the new elements and animations. Future needs are for the disposition of the elements via CSS. Future step is to integrate the light/dark button to switch to one piece view.

--- script.js
+++ script.js
@@ -2,11 +2,22 @@ let currentSlide = 0;
 let oldSlide = 0;
 const slides = document.querySelectorAll('.slide');
 const dots = document.querySelectorAll('.dot');
+const dates = document.querySelectorAll('.date');
 let isAnimating = false;
 
 function showSlide(index) {
     if (isAnimating || index == currentSlide) return;  // Si une animation est en cours, ne rien faire
     isAnimating = true;
+
+    dates.forEach(function callback(item, itemIndex) {
+        const spans = item.querySelectorAll('span');
+        // Itérer sur chaque <span> à l'intérieur de cet élément
+        spans.forEach(span => {
+            // Faire quelque chose avec chaque <span>
+            console.log(span);
+            span.classList.remove('transition');
+        });
+    });
 
     if (index>currentSlide){
         // Si on va à droite, on ajoute les classes 'previousleft' pour indiquer que la diapo actuelle part à gauche
@@ -16,7 +27,6 @@ function showSlide(index) {
         slides.forEach(function callback(item, itemIndex) {
             if (index <= itemIndex || currentSlide == itemIndex) {return};
             slides[itemIndex].classList.remove('previousright');
-            slides[itemIndex].classList.add('toleft');
         });
     } else {
         // Si on va à gauche, on ajoute les classes 'previousright' pour indiquer que la diapo actuelle part à droite
@@ -26,7 +36,6 @@ function showSlide(index) {
         slides.forEach(function callback(item, itemIndex) {
             if (index >= itemIndex || currentSlide == itemIndex) {return};
             slides[itemIndex].classList.remove('previousleft');
-            slides[itemIndex].classList.remove('toleft');
         });
     }
 
@@ -38,8 +47,18 @@ function showSlide(index) {
     currentSlide = index;
     
     // la nouvelle slide est maintenant la slide actuelle
+    const new_spans = dates[currentSlide].querySelectorAll('span');
+    const old_spans = dates[oldSlide].querySelectorAll('span');
+    new_spans.forEach(function callback(span, spanindex) {
+        if (span.textContent == old_spans[spanindex].textContent) {return};
+        span.classList.add('transition');
+        old_spans[spanindex].classList.add('transition');
+    });
     slides[currentSlide].classList.add('active');
-    dots[currentSlide].classList.add('active');
+    
+    for (let i = 0; i <= currentSlide; i++) {
+        dots[i].classList.add('active');
+    }
 
     // Utiliser un délai pour empêcher les animations de se chevaucher
     setTimeout(() => {            
@@ -47,6 +66,20 @@ function showSlide(index) {
     }, 300); // Assurez-vous que ce délai correspond à la durée de la transition dans le CSS
 
 }
+
+document.addEventListener('keydown', function(event) {
+    // Vérifier si la touche "flèche gauche" est pressée
+    if (event.key === 'ArrowLeft') {
+        if (currentSlide > 0) {
+            showSlide(currentSlide - 1);
+        }
+    }
+    if (event.key === 'ArrowRight') {
+        if (currentSlide < slides.length - 1) {
+            showSlide(currentSlide + 1);
+        }
+    }
+});
 
 // Initialiser le carrousel en affichant la première diapositive
 showSlide(0);
