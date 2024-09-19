0956605fe1294b88dc8f23ffcced8e423f010cd2
emericriv <e19riviere@gmail.com>
2024-09-19 11:43:07 +0200

    sliding mode bug
    when going to right, left, then right and changing mode, the previous slide were not going to the left as expected, now corrected.
    Adding some comments and changed title of the page.

--- script.js
+++ script.js
@@ -1,34 +1,54 @@
+// Constants to handle slides and slide index
 let currentSlide = 0;
 let oldSlide = 0;
 const dbzSlides = document.querySelectorAll('.slide.dbz');
 const opSlides = document.querySelectorAll('.slide.op');
 const slides = document.querySelectorAll('.slide');
 let selectedSlides = dbzSlides; 
+
+// Same for the dates to handle the transition class
 const dbzDates = document.querySelectorAll('.dbz .date');
 const opDates = document.querySelectorAll('.op .date');
 const dates = document.querySelectorAll('.date');
 let selectedDates = dbzDates;
+
+// Constants to handle the dots and arrow state (enabled or disabled)
 const dots = document.querySelectorAll('.dot');
 const rightArrow = document.querySelector('.right-arrow');
 const leftArrow = document.querySelector('.left-arrow');
+
+// isAnimating is a flag to prevent multiple animations at the same time
 let isAnimating = false;
+
+// constants to handle elements for the switches between the themes
 const themeToggle = document.getElementById('themeToggle');
 const themeText = document.getElementById('themeText');
 const selectionDiv = document.querySelector('.selection');
 const body = document.body;
+
+// Allows to know which theme is currently displayed
 let isOpMode = false;
+
+// Used to prevent from changing the theme while an animation is in progress
 const checkbox = document.getElementById('themeToggle');
 
 function showSlide(index) {
-    if (isAnimating) return;  // Si une animation est en cours, ne rien faire
-    isAnimating = true;
+    // if an animation is in progress, do nothing
+    if (isAnimating) return;
 
+    // disable the switch while the animation is in progress
+    isAnimating = true; 
+
+    // shift left is used later to shift some slides to left without transition
+    // change this class here to shift left allows to keep the previous slide in the right position
+    // and use the right class for the good behavior of the carousel
     let shiftedSlides = document.querySelectorAll('.shiftleft');
     shiftedSlides.forEach(function callback(item, itemIndex) {
         item.classList.add('previousleft');
         item.classList.remove('shiftleft');
     });
 
+    // remove transition class from all date spans
     dates.forEach(function callback(item, itemIndex) {
         const spans = item.querySelectorAll('span');
         // Itérer sur chaque <span> à l'intérieur de cet élément
@@ -38,24 +58,31 @@ function showSlide(index) {
         });
     });
 
-    // la slide actuelle est maintenant la slide précédente
+    // remove the active class from the current displayed slide
     selectedSlides[currentSlide].classList.remove('active');
     dots[currentSlide].classList.remove('active');
 
     if (index>currentSlide){
-        // Si on va à droite, on ajoute les classes 'previousleft' pour indiquer que la diapo actuelle part à gauche
+        // current slide need to go to the left
+        // removing the ^revious right class is just a precaution
         selectedSlides[currentSlide].classList.remove('previousright');
         selectedSlides[currentSlide].classList.add('previousleft');
     } else if (index<currentSlide){
-        // Si on va à gauche, on ajoute les classes 'previousright' pour indiquer que la diapo actuelle part à droite
+        // Same for the other side
         selectedSlides[currentSlide].classList.remove('previousleft');
         selectedSlides[currentSlide].classList.add('previousright');
     } else {
-        // on change de mode
+        // Switching mode (or first iteration)
+        // slide the current slide to the left and remove the previous right class
+        selectedSlides[currentSlide].classList.remove('previousright');
         selectedSlides[currentSlide].classList.add('previousleft');
+
+        // Change selected slides as the mode switched
         if (isOpMode && selectedSlides == dbzSlides) {
             opSlides.forEach(function callback(item, itemIndex) {
-                if (itemIndex <= index) {item.classList.add('shiftleft');}
+                // Put to left slides that have index lower than the current to have a good behavior
+                // on the new slides
+                if (itemIndex < index) {item.classList.add('shiftleft');}
             });
             selectedSlides = opSlides;
             selectedDates = opDates;
@@ -68,6 +95,7 @@ function showSlide(index) {
         }
     }
     
+    // Switching to the nex slide
     oldSlide = currentSlide;
     currentSlide = index;
     
@@ -84,23 +112,31 @@ function showSlide(index) {
         leftArrow.classList.remove('disabled');
     }
     
-    // la nouvelle slide est maintenant la slide actuelle
+    // Getting dates span from the previous slide and the new one to prepare transition
     const new_spans = selectedDates[currentSlide].querySelectorAll('span');
     const old_spans = selectedDates[oldSlide].querySelectorAll('span');
+
+    // Adding transition class to both old and new dates 
     new_spans.forEach(function callback(span, spanindex) {
         if (span.textContent == old_spans[spanindex].textContent) {return};
         span.classList.add('transition');
         old_spans[spanindex].classList.add('transition');
     });
+
+    // Adding active class to the slide to display
     selectedSlides[currentSlide].classList.add('active');
     
+    // Adding the active class to the associated dot, and the previous ones
     for (let i = 0; i <= currentSlide; i++) {
         dots[i].classList.add('active');
     }
 
-    // Utiliser un délai pour empêcher les animations de se chevaucher
+    // Timeout to allow new change once all animations are done (after 300ms)
     setTimeout(() => {            
         isAnimating = false;
+
+        // remove all classes from the undisplayed slides
+        // Need to do it after the timer to don't have misbehavior while changing
         if (selectedSlides == dbzSlides) {
             opSlides.forEach(function callback(item, itemIndex) {
                 item.classList.remove('shiftleft');
@@ -113,13 +149,15 @@ function showSlide(index) {
                 item.classList.remove('previousleft');
                 item.classList.remove('previousright');
             });
-        } 
+        }
+
+        // enable changing mode checkbox (disabled when changing mode)
         checkbox.disabled = false;
-    }, 300); // Assurez-vous que ce délai correspond à la durée de la transition dans le CSS
+    }, 300);
 }
 
 document.addEventListener('keydown', function(event) {
-    // Vérifier si la touche "flèche gauche" est pressée
+    // Allow user to use the carousel with the left and right arrow keys
     if (event.key === 'ArrowLeft') {
         if (currentSlide > 0) {
             showSlide(currentSlide - 1);
@@ -133,24 +171,29 @@ document.addEventListener('keydown', function(event) {
 });
 
 function showNextSlide() {
+    // When the user click on the right arrow, the next slide is displayed
     if (currentSlide < slides.length - 1) {
         showSlide(currentSlide + 1);
     }
 }
 
 function showPreviousSlide() {
+    // When the user click on the left arrow, the previous slide is displayed
     if (currentSlide > 0) {
         showSlide(currentSlide - 1);
     }
 }
 
 themeToggle.addEventListener('change', () => {
+    // Disable the switch while the animation is in progress (enable back at the end of this one)
     checkbox.disabled = true;
+    // Toggle the theme
     body.classList.toggle('op-mode');
     selectionDiv.classList.toggle('op-mode');
     isOpMode = !isOpMode;
+    // Show slide from the new theme
     showSlide(currentSlide);
 });
 
-// Initialiser le carrousel en affichant la première diapositive
+// Init carousel with the first slide
 showSlide(0);
