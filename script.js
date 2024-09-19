1dfc878592f4a44f709c4b99eea074efaae65267
emericriv <e19riviere@gmail.com>
2024-09-19 11:01:20 +0200

    Main task finished
    Well integrated one piece mode with the slider to switch between the two modes
    Well integrated one piece slides
    Integrated timings in order to prevent switches in the same 300ms
    Well hendled transition between one piece slides and dbz slides so there is always a transition.
    future modifications will be to optimize or comment the code, change some placements, and maybe try to integrate the responsive behavior.

--- script.js
+++ script.js
@@ -1,60 +1,98 @@
 let currentSlide = 0;
 let oldSlide = 0;
+const dbzSlides = document.querySelectorAll('.slide.dbz');
+const opSlides = document.querySelectorAll('.slide.op');
 const slides = document.querySelectorAll('.slide');
-const dots = document.querySelectorAll('.dot');
+let selectedSlides = dbzSlides; 
+const dbzDates = document.querySelectorAll('.dbz .date');
+const opDates = document.querySelectorAll('.op .date');
 const dates = document.querySelectorAll('.date');
+let selectedDates = dbzDates;
+const dots = document.querySelectorAll('.dot');
+const rightArrow = document.querySelector('.right-arrow');
+const leftArrow = document.querySelector('.left-arrow');
 let isAnimating = false;
+const themeToggle = document.getElementById('themeToggle');
+const themeText = document.getElementById('themeText');
+const selectionDiv = document.querySelector('.selection');
+const body = document.body;
+let isOpMode = false;
+const checkbox = document.getElementById('themeToggle');
 
 function showSlide(index) {
-    if (isAnimating || index == currentSlide) return;  // Si une animation est en cours, ne rien faire
+    if (isAnimating) return;  // Si une animation est en cours, ne rien faire
     isAnimating = true;
+
+    let shiftedSlides = document.querySelectorAll('.shiftleft');
+    shiftedSlides.forEach(function callback(item, itemIndex) {
+        item.classList.add('previousleft');
+        item.classList.remove('shiftleft');
+    });
 
     dates.forEach(function callback(item, itemIndex) {
         const spans = item.querySelectorAll('span');
         // Itérer sur chaque <span> à l'intérieur de cet élément
         spans.forEach(span => {
             // Faire quelque chose avec chaque <span>
-            console.log(span);
             span.classList.remove('transition');
         });
     });
 
+    // la slide actuelle est maintenant la slide précédente
+    selectedSlides[currentSlide].classList.remove('active');
+    dots[currentSlide].classList.remove('active');
+
     if (index>currentSlide){
         // Si on va à droite, on ajoute les classes 'previousleft' pour indiquer que la diapo actuelle part à gauche
-        slides[currentSlide].classList.remove('previousright');
-        slides[currentSlide].classList.add('previousleft');
-        // Si un saut de la diapo 0 à la diapo 2 est effectué, les diapos 0 et 1 doivent partir à gauche
-        slides.forEach(function callback(item, itemIndex) {
-            if (index <= itemIndex || currentSlide == itemIndex) {return};
-            slides[itemIndex].classList.remove('previousright');
-        });
-    } else {
+        selectedSlides[currentSlide].classList.remove('previousright');
+        selectedSlides[currentSlide].classList.add('previousleft');
+    } else if (index<currentSlide){
         // Si on va à gauche, on ajoute les classes 'previousright' pour indiquer que la diapo actuelle part à droite
-        slides[currentSlide].classList.remove('previousleft');
-        slides[currentSlide].classList.add('previousright');
-        // Si un saut de la diapo 2 à la diapo 0 est effectué, les diapos 1 et 2 doivent partir à droite
-        slides.forEach(function callback(item, itemIndex) {
-            if (index >= itemIndex || currentSlide == itemIndex) {return};
-            slides[itemIndex].classList.remove('previousleft');
-        });
+        selectedSlides[currentSlide].classList.remove('previousleft');
+        selectedSlides[currentSlide].classList.add('previousright');
+    } else {
+        // on change de mode
+        selectedSlides[currentSlide].classList.add('previousleft');
+        if (isOpMode && selectedSlides == dbzSlides) {
+            opSlides.forEach(function callback(item, itemIndex) {
+                if (itemIndex <= index) {item.classList.add('shiftleft');}
+            });
+            selectedSlides = opSlides;
+            selectedDates = opDates;
+        } else {
+            dbzSlides.forEach(function callback(item, itemIndex) {
+                if (itemIndex < index) {item.classList.add('shiftleft');}
+            });
+            selectedSlides = dbzSlides;
+            selectedDates = dbzDates;
+        }
     }
-
-    // la slide actuelle est maintenant la slide précédente
-    slides[currentSlide].classList.remove('active');
-    dots[currentSlide].classList.remove('active');
     
     oldSlide = currentSlide;
     currentSlide = index;
     
+    // disable left arrow if currentSlide is 0
+    if (currentSlide == selectedSlides.length - 1) {
+        rightArrow.classList.add('disabled');
+    } else {
+        rightArrow.classList.remove('disabled');
+    }
+    // disable right arrow if currentSlide is last
+    if (currentSlide == 0) {
+        leftArrow.classList.add('disabled');
+    } else {
+        leftArrow.classList.remove('disabled');
+    }
+    
     // la nouvelle slide est maintenant la slide actuelle
-    const new_spans = dates[currentSlide].querySelectorAll('span');
-    const old_spans = dates[oldSlide].querySelectorAll('span');
+    const new_spans = selectedDates[currentSlide].querySelectorAll('span');
+    const old_spans = selectedDates[oldSlide].querySelectorAll('span');
     new_spans.forEach(function callback(span, spanindex) {
         if (span.textContent == old_spans[spanindex].textContent) {return};
         span.classList.add('transition');
         old_spans[spanindex].classList.add('transition');
     });
-    slides[currentSlide].classList.add('active');
+    selectedSlides[currentSlide].classList.add('active');
     
     for (let i = 0; i <= currentSlide; i++) {
         dots[i].classList.add('active');
@@ -62,9 +100,22 @@ function showSlide(index) {
 
     // Utiliser un délai pour empêcher les animations de se chevaucher
     setTimeout(() => {            
-        isAnimating = false; 
+        isAnimating = false;
+        if (selectedSlides == dbzSlides) {
+            opSlides.forEach(function callback(item, itemIndex) {
+                item.classList.remove('shiftleft');
+                item.classList.remove('previousleft');
+                item.classList.remove('previousright');
+            });
+        } else {
+            dbzSlides.forEach(function callback(item, itemIndex) {
+                item.classList.remove('shiftleft');
+                item.classList.remove('previousleft');
+                item.classList.remove('previousright');
+            });
+        } 
+        checkbox.disabled = false;
     }, 300); // Assurez-vous que ce délai correspond à la durée de la transition dans le CSS
-
 }
 
 document.addEventListener('keydown', function(event) {
@@ -79,6 +130,26 @@ document.addEventListener('keydown', function(event) {
             showSlide(currentSlide + 1);
         }
     }
+});
+
+function showNextSlide() {
+    if (currentSlide < slides.length - 1) {
+        showSlide(currentSlide + 1);
+    }
+}
+
+function showPreviousSlide() {
+    if (currentSlide > 0) {
+        showSlide(currentSlide - 1);
+    }
+}
+
+themeToggle.addEventListener('change', () => {
+    checkbox.disabled = true;
+    body.classList.toggle('op-mode');
+    selectionDiv.classList.toggle('op-mode');
+    isOpMode = !isOpMode;
+    showSlide(currentSlide);
 });
 
 // Initialiser le carrousel en affichant la première diapositive
